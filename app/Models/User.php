00b0f8a7e34d698b6e8a56da5df207bf5db52175
Ramiruuu <oclaritremar25@gmail.com>
2025-03-02 11:41:04 +0800

    added

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $table = 'tbl_user';
+    protected $table = 'users';
 
     // Columns sa table
     protected $fillable = [
