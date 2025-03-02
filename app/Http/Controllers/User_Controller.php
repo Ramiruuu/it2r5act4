2bd20dbf56a63e8934db2bcd8347f6c29675045c
Ramiruuu <oclaritremar25@gmail.com>
2025-03-02 09:51:37 +0800

    modefied

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -17,12 +17,12 @@ class UserController extends Controller {
         $users = User::all();
         return $this->response($users, 200);
         
-        $users = DB::connection('mysql')
-        ->select("Select * from tbluser where userid <= 3");
+        // users = DB::connection('mysql')
+        // ->select("Select * from tbluser where userid <= 3");
 
         // return response()->json($users, 200);
         // return response()->json(['data' => $users, 'site' => 1], 200);
-        return response()->json(['data' => $users], 200);
+        // return response()->json(['data' => $users], 200);
         // return $this->successResponse($users);  
     }
 }
