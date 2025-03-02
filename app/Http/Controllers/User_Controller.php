6d4d312504055af7821f3e2213042c67667c0cde
Ramiruuu <oclaritremar25@gmail.com>
2025-03-02 09:17:53 +0800

    Changes in code

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -1,32 +1,19 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request; 
+use Illuminate\Http\Request;
 use App\User;
 
-class UserController extends Controller 
-{     
+class UserController extends Controller {
     private $request;
 
-    public function __construct(Request $request)
-    {
+    public function __construct(Request $request) {
         $this->request = $request;
     }
 
-    /**
-     * Return the list of users
-     * @return Illuminate\Http\Response
-     */
-    public function getUsers()
-    {         
+    public function getUsers() {
         $users = User::all();
-        return response()->json($users, 200);
-    }
-
-    public function index()
-    {
-        $users = User::all();
-        return $this->successResponse($users);
+        return $this->response($users, 200);
     }
 }
